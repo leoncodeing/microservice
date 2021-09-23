e4f2b016f3774187f7100005b865d2be3c522cc5
leon <fabulus2013.cs@gmail.com>
2021-09-23 19:14:15 +0800

    redis access
    redis access

---
+++ src/main/java/annotation/Desc.java
@@ -0,0 +1,11 @@
+package annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(value = RetentionPolicy.CLASS)
+public @interface Desc {
+    String value();
+}
---
+++ src/main/java/cache/RedisTemplate.java
@@ -0,0 +1,9 @@
+package cache;
+
+import annotation.Desc;
+
+@Desc(value = "redis")
+public class RedisTemplate {
+
+
+}
